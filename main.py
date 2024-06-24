ab6067b8b0389fd5ccb2174e72fb8038cca3fd17
UrokiVV <vvsoft@yahoo.com>
2024-06-24 15:05:13 +0300

    correct main Screen 24.06

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 # main  Game "Tir"  22.06.2024
-# import pygame
-# pygame.init()
+import pygame
+pygame.init()
 
 SCREEN_SIZE_X = 800
 SCREEN_SIZE_Y = 600
+screen = pygame.display.set_mode((SCREEN_SIZE_X, SCREEN_SIZE_Y))
+pygame.display.set_caption("Игра Тир")
+
