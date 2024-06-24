9b3eb7c96355a068fa832da15db95a1af5cc4e01
UrokiVV <vvsoft@yahoo.com>
2024-06-24 14:58:39 +0300

    correct main 24.06

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
 # main  Game "Tir"  22.06.2024
-import pygame
-pygame.init()
+# import pygame
+# pygame.init()
 
-print("Game Tir")
-fl_run = True
-while fl_run:
-    pass
-
-pygame.quit()
+SCREEN_SIZE_X = 800
+SCREEN_SIZE_Y = 600
