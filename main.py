aeea843455183aa372ea24feb388bd2f0d8b8b00
UrokiVV <vvsoft@yahoo.com>
2024-06-24 21:46:34 +0300

    main cycle +target.png

--- main.py
+++ main.py
@@ -8,10 +8,10 @@ SCREEN_SIZE_X = 800
 SCREEN_SIZE_Y = 600
 screen = pygame.display.set_mode((SCREEN_SIZE_X, SCREEN_SIZE_Y))
 pygame.display.set_caption("Игра Тир")
-icon = pygame.image.load("img / icon_N1.jpg")
+icon = pygame.image.load("img/icon_N1.jpg")
 pygame.display.set_icon(icon)
 
-target_image  = pygame.image.load("img /")
+target_image  = pygame.image.load("img/target.png")
 target_width  = 80
 target_height = 80
 target_x = random.randint(0, SCREEN_SIZE_X - target_width)
@@ -19,6 +19,18 @@ target_y = random.randint(0, SCREEN_SIZE_Y - target_height)
 
 fl_run = True
 while fl_run:
-    pass
+    for event in pygame.event.get():
+        if event.type == pygame.quit:
+            fl_run = False
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            mouse_x, mouse_y = pygame.mouse.get_pos()
+            if target_x < mouse_x < target_x+target_width and target_y < mouse_y < target_y+target_height:
+                target_x = random.randint(0, SCREEN_SIZE_X - target_width)
+                target_y = random.randint(0, SCREEN_SIZE_Y - target_height)
+                screen.blit(target_image, (target_x, target_y))
+                pygame.display.update()
+
 
 pygame.quit()
+
+
