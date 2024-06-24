c43ea381e5b57f840f204a2540fc1ec271ce8c50
UrokiVV <vvsoft@yahoo.com>
2024-06-24 22:31:06 +0300

    main Final

--- main.py
+++ main.py
@@ -9,7 +9,6 @@ SCREEN_SIZE_Y = 600
 screen = pygame.display.set_mode((SCREEN_SIZE_X, SCREEN_SIZE_Y))
 pygame.display.set_caption("Игра Тир")
 color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
-screen.fill(color)
 
 icon = pygame.image.load("img/icon_N1.jpg")
 pygame.display.set_icon(icon)
@@ -18,15 +17,17 @@ target_width  = 80
 target_height = 80
 target_x = random.randint(0, SCREEN_SIZE_X - target_width)
 target_y = random.randint(0, SCREEN_SIZE_Y - target_height)
+screen.fill(color)
+
 screen.blit(target_image, (target_x, target_y))
 pygame.display.update()
-print("event=" + str(pygame.quit))
+#print("event=" + str(pygame.quit))
 
 
 fl_run = True
 while fl_run:
+    screen.fill(color)
     for event in pygame.event.get():
-        # print("event=" +str(event.type))
         if event.type == pygame.QUIT:
             print("quit")
             fl_run = False
